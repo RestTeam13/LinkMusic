3ab1a08d13462c5bed0c04aa09e24dab58c1e80c
Rest133 <31849309+Rest133@users.noreply.github.com>
2021-10-12 01:25:57 +0300

    Initial commit

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles.css'
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div className="app">
+        Дед лайн :D
+      <header className="app-header">
+          <img src={logo} alt="logo"/>Дед ролл
+      </header>
+    </div>
+  );
+}
+
+export default App;
