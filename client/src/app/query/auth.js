93ba7a2f9d4f75b31a201ef08e7a040c90674cfa
Rest133 <dubrovinanatolii@mail.ru>
2022-01-26 06:57:56 +0300

    Change dev dependencies

--- client/src/app/query/auth.js
+++ client/src/app/query/auth.js
@@ -1,10 +1,10 @@
 import {gql} from '@apollo/client'
 
 
-// export const AUTH = gql`
-//     mutation ($email: String, $password: String){
-//         authenticateUserWithPassword(email: $email, password: $password){
-//             token
-//         }
-//     }
-// `
+export const AUTH = gql`
+#    mutation ($email: String, $password: String){
+#        authenticateUserWithPassword(email: $email, password: $password){
+#            token
+#        }
+#    }
+`
