1105f20d2d7195edc653cd2ba330add16a313a24
Rest133 <dubrovinanatolii@mail.ru>
2022-02-09 09:54:19 +0300

    Add react app to keystone.js

--- index.js
+++ index.js
@@ -4,56 +4,61 @@ const {KnexAdapter} = require('@keystonejs/adapter-knex')
 const {PasswordAuthStrategy} = require('@keystonejs/auth-password');
 const {AdminUIApp} = require('@keystonejs/app-admin-ui');
 const {GraphQLApp} = require('@keystonejs/app-graphql');
-const {StaticApp} = require('@keystonejs/app-static')
 const {createImg} = require('./apps/createCaptcha')
 const UserSchema = require('./lists/User')
 const NewsArticleSchema = require('./lists/NewsArtice')
 
 const keystone = new Keystone({
-  adapter: new KnexAdapter({
-    knexOptions: {
-      client: process.env.CLIENT,
-      connection: {
-        host: process.env.HOSTDB,
-        port: process.env.PORTDB,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE
-      }
-    }
-  }),
-  cookieSecret: process.env.COOKIE_SECRET
+    adapter: new KnexAdapter({
+        knexOptions: {
+            client: process.env.CLIENT,
+            connection: {
+                host: process.env.HOSTDB,
+                port: process.env.PORTDB,
+                user: process.env.USER,
+                password: process.env.PASSWORD,
+                database: process.env.DATABASE
+            }
+        }
+    }),
+    cookieSecret: process.env.COOKIE_SECRET
 })
 
 keystone.createList('User', UserSchema)
 keystone.createList('NewsArticle', NewsArticleSchema)
 
 keystone.extendGraphQLSchema({
-  queries: [
-    {
-      schema: 'getCaptcha: String!',
-      resolver: (_, {}) => createImg(),
-    },
-  ]
+    queries: [
+        {
+            schema: 'getCaptcha: String!',
+            resolver: (_, {}) => createImg(),
+        },
+    ]
 })
 
 const authStrategy = keystone.createAuthStrategy({
-  type: PasswordAuthStrategy,
-  list: 'User',
+    type: PasswordAuthStrategy,
+    list: 'User',
 })
 
 module.exports = {
-  keystone,
-  apps: [
-    new GraphQLApp(),
-    new AdminUIApp({
-      name: 'Link Music',
-      authStrategy
-    }),
-    new StaticApp({
-      path: '/',
-      src: 'public',
-      fallback: 'nf.html',
-    }),
-  ],
+    configureExpress: app => {
+        const express = require('express')
+        const path = require('path')
+        if (process.env.NODE_ENV === 'production') {
+            app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+            app.get('*', (req, res) => {
+                res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+            })
+        }
+    },
+    keystone,
+    apps: [
+        new GraphQLApp(),
+        new AdminUIApp({
+            name: 'Link Music',
+            authStrategy
+        }),
+    ],
 };
